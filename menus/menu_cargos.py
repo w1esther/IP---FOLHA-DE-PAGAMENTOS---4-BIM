2e39441b7e9af6fcfe8d9e89b4f51a1f07ca8a23
georgecrsete <george.p@escolar.ifrn.edu.br>
2026-01-09 13:14:24 -0300

    upp

--- menus/menu_cargos.py
+++ menus/menu_cargos.py
@@ -1,4 +1,28 @@
-def menu_cargos():
-    print('\n\n Selecione o número correspondente à operação que deseja realizar com os cargos: \n\n - 1 Criar \n\n - 2 Listar \n\n - 3 Atualizar \n\n - 4 Remover \n\n - 0 Sair do sistema \n\n')
+from CRUDs.cargos import criar_cargo, listar_cargos, buscar_cargo_por_id
 
-    opcao_cargos = int(input('Digite a operação desejada: '))
+def menu_cargos():
+    while True:
+     
+        print('\n\n Selecione o número correspondente à operação que deseja realizar com os cargos: \n\n'
+              ' - 1 Criar \n\n'
+              ' - 2 Listar \n\n'
+              ' - 3 Buscar por ID \n\n'
+              ' - 0 Sair do sistema \n\n')
+
+        try:
+            opcao_cargos = int(input("Digite a opção: "))
+        except ValueError:
+            print("\nDigite apenas números!\n")
+            continue 
+
+        if opcao_cargos == 1:
+            criar_cargo()
+
+        elif opcao_cargos == 2:
+            listar_cargos()
+
+        elif opcao_cargos == 3:
+            buscar_cargo_por_id()
+
+        elif opcao_cargos == 0:
+            break
