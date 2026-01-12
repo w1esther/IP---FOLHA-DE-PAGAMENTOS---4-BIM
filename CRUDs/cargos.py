93803ca34461c2da2583b928806e0bc886cc8c0a
w1esther <w1mariaesther@gmail.com>
2026-01-12 11:38:31 -0300

    corrigindo erros

--- CRUDs/cargos.py
+++ CRUDs/cargos.py
@@ -112,9 +112,4 @@ def remover_cargo():
             salvar_json(CAMINHO_CARGOS, cargos)
             print("\nCargo removido com sucesso!\n")
             return
-
-<<<<<<< HEAD
     print("Cargo não encontrado.\n")
-=======
-    print("Cargo não encontrado.\n")
->>>>>>> 86ce74f77e74f3e7c495019579243701ca855bd6
